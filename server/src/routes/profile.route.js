ba9165501011b29a2b1c69a2d6bccfd2723cf1d0
savi-1311 <shambhavishandilya01@gmail.com>
2021-11-11 17:51:14 +0530

    Added score feature

--- server/src/routes/profile.route.js
+++ server/src/routes/profile.route.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const passport = require('passport');
 const { fetchProfile, updateProfile } = require('../controllers/profile.controller');
+const { fetchScore} = require('../controllers/leaderboard.controller');
 const profileRouter = Router();
 
 // Profile router for handling requests from clients
 profileRouter.post('/fetch-profile', fetchProfile);
+profileRouter.post('/fetch-score', fetchScore);
 profileRouter.put('/update-profile', passport.authenticate('jwt', { session: false }), updateProfile);
 
 module.exports = profileRouter;
