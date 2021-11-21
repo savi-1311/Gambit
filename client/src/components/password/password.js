a8c8867e4e5bffa93ff7146063e25f42f8adbc18
Ishita Choudhary <ishitachoudhary270900@gmail.com>
2021-11-21 20:00:20 +0530

    added password component

---
+++ client/src/components/password/password.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import UpdatePassword from './update-password';
+import ResetPassword from './reset-password';
+import './password.css';
+
+export {
+    UpdatePassword,
+    ResetPassword
+};
